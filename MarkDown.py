c8e2a3471a66b607eee33f034745471f1666eb1b
Negar Haghpanahi <59286942+Negar-Haghpanahi@users.noreply.github.com>
2024-05-13 16:14:42 +0330

    Update MarkDown.py

--- MarkDown.py
+++ MarkDown.py
@@ -19,16 +19,32 @@ class MarkdownText():
         soup = BeautifulSoup(html_content, "html.parser")
         
         # Convert <h1> tags to Markdown headings
+        markdown_text = soup.get_text(separator="\n")
+        # Find <h1> tag with specific class
+        h1_tag = soup.find("h1", class_="Title3D")
+        
+        # Extract text content from <span> inside <h1>
+        if h1_tag:
+            title_text = h1_tag.get_text(strip=True)
+            markdown_text += f"\n\n## {title_text}\n\n"
+        
         for h1_tag in soup.find_all("h1"):
             h1_tag.string = f"# {h1_tag.string}\n\n"
-        
+
         # Convert <br> tags to Markdown line breaks
         for br_tag in soup.find_all("br"):
             br_tag.replace_with("\n\n")
-        
+
+        for h1_b in  soup.find_all("b", text="پیام:"):
+            h1_b.string = f"## {h1_b.string}\n\n"
+     
         # Remove any remaining HTML tags
-        markdown_text = soup.get_text(separator="\n")
         
+        
+        # Find elements with specified style attribute
+
+      
+
         self.markdown_output += markdown_text + "\n\n" 
         return self.markdown_output
             
